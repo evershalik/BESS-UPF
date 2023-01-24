f702be5b4f92d8f4ba29a6c4c70ce87ac15a3fda
SHANKAR <98207888+evershalik@users.noreply.github.com>
2023-01-24 14:27:26 +0530

    Update bess.sh

--- bess.sh
+++ bess.sh
@@ -45,7 +45,7 @@ sed -i 's/#mode="sim"/mode="sim"/'  /home/ubuntu/upf/scripts/docker_setup.sh
 
 cd /home/ubuntu/upf/
 sudo ./scripts/docker_setup.sh
-wait
+
 docker exec bess-pfcpiface pfcpiface -config conf/upf.json -simulate create
 docker exec bess ./bessctl run up4
 docker exec bess ./bessctl show pipeline > pipeline.txt
